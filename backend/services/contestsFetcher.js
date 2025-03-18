f7d4347989a42de055a194bb48a0cc253a58c1fa
ashutosh229 <akumarjha875@gmail.com>
2025-03-19 01:39:17 +0530

    added

--- backend/services/contestsFetcher.js
+++ backend/services/contestsFetcher.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { all } from "axios";
 
 export const contestFetcher = async () => {
   try {
@@ -38,7 +38,61 @@ export const contestFetcher = async () => {
 
     const allContests = [...codeforcesContests, ...codechefContests];
 
-    return allContests;
+    const codeforcesResponseForPastContests = await fetch(
+      "https://codeforces.com/api/contest.list",
+      { method: "GET" }
+    );
+    const dataCodeforcesForPastContests =
+      await codeforcesResponseForPastContests.json();
+    const currentTime = Math.floor(Date.now() / 1000); // Get current timestamp in secondass
+    const oneWeekAgo = currentTime - 7 * 24 * 60 * 60; // One week ago timestamp
+    const codeforcesContestsForPast = dataCodeforcesForPastContests.result
+      .filter(
+        (contest) =>
+          contest.phase === "FINISHED" && contest.startTimeSeconds >= oneWeekAgo
+      )
+      .map((contest) => ({
+        platform: "Codeforces",
+        name: contest.name,
+        startTime: new Date(contest.startTimeSeconds * 1000),
+        duration: contest.durationSeconds,
+        url: `https://codeforces.com/contest/${contest.id}`,
+      }));
+
+    const codechefResponseForPastContests = await fetch(
+      "https://www.codechef.com/api/list/contests/all?sort_by=START&sorting_order=asc&offset=0&mode=all",
+      {
+        method: "GET",
+      }
+    );
+    const dataCodechefForPastContests =
+      await codechefResponseForPastContests.json();
+    const currentTime2 = new Date();
+    const oneWeekAgo2 = new Date(currentTime2);
+    oneWeekAgo2.setDate(currentTime2.getDate() - 7); // Subtract 7 days
+
+    const codechefContestsForPast = [
+      ...dataCodechefForPastContests.past_contests,
+    ]
+      .filter((contest) => {
+        const contestEndDate = new Date(contest.contest_end_date_iso);
+        return contestEndDate >= oneWeekAgo && contestEndDate < currentTime;
+      })
+      .map((contest) => ({
+        platform: "CodeChef",
+        name: contest.contest_name,
+        startTime: new Date(contest.contest_start_date),
+        duration: Number(contest.contest_duration) / 60,
+        url: `https://www.codechef.com/${contest.contest_code}`,
+      }));
+
+    const allContestsForPast = [
+      ...codeforcesContestsForPast,
+      ...codechefContestsForPast,
+    ];
+
+    const contests = [...allContests, ...allContestsForPast];
+    return contests;
   } catch (error) {
     console.log(error);
     throw error;
---
+++ todo.md
@@ -0,0 +1,9 @@
+# To-Do List
+
+## Features to Implement
+- [ ] show the past contests in the last 1 week
+- [ ] filter implementation for the past contests 
+- [ ] bookmark no implementation for the past 1 week contests 
+- [ ] link population for youtube solution for past contest (bonus)
+- [ ] manual addition of youtube links (alternative but no bonus)
+- [ ] reminder functionality
