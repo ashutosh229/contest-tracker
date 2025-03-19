d29496dbd2ab107a33bb2aa42b3764edc13a23c7
ashutosh229 <akumarjha875@gmail.com>
2025-03-19 06:01:20 +0530

    added navigationt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,2 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import home
+import Home from "./pages/Home";
+import ReminderForm from "./pages/ReminderForm";
+import ReminderList from "./pages/ReminderList";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/reminder-form",
+    element: <ReminderForm></ReminderForm>,
+  },
+  {
+    path: "/reminder-list",
+    element: <ReminderList></ReminderList>,
+  },
+];
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {routes.map((route) => (
+          <Route path={route.path} element={route.element}></Route>
+        ))}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
